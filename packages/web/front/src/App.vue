f24084d0e88f56fc1607d110abeb78a972801f5c
Mathieu Ledru <matyo91@gmail.com>
2020-08-20 16:31:25 +0200

    :construction: add packages

---
+++ packages/web/front/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app" class="h-screen w-full flex justify-center items-center bg-green-900">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
