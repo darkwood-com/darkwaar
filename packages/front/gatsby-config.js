d288fd3aaf47602aecbd5a2c8833ce10ce1021e8
Mathieu Ledru <matyo91@gmail.com>
2020-08-23 21:58:11 +0200

    :rocket: deploy

--- packages/front/gatsby-config.js
+++ packages/front/gatsby-config.js
@@ -1,5 +1,5 @@
 const projectPath = './'
-const docsPath = '../../docs'
+const docsPath = process.env.DOCS_PATH || '../../docs'
 const activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development'
 
 console.log(`Using environment config: '${activeEnv}'`)
