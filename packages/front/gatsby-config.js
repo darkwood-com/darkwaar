f7859fbeb2ffcdbdabc48958baa7be5f080102d6
Mathieu Ledru <matyo91@gmail.com>
2020-11-26 18:17:25 +0100

    rss update

--- packages/front/gatsby-config.js
+++ packages/front/gatsby-config.js
@@ -94,6 +94,7 @@ module.exports = {
                               nodes {
                                 id
                                 excerpt
+                                html
                                 fields {
                                   slug
                                 }
@@ -141,6 +142,7 @@ module.exports = {
                   description: node.excerpt,
                   url: `${siteMetadata.siteUrl}/blog/${node.fields.slug}`,
                   guid: `${siteMetadata.siteUrl}/blog/${node.fields.slug}`,
+                  custom_elements: [{ "content:encoded": node.html }],
                   categories: node.frontmatter.tags,
                   author: node.frontmatter.author.name,
                   date: node.frontmatter.date,
