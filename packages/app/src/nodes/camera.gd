6152c6e48061689751cdd7ddea2e5e414f5b4706
Mathieu Ledru <matyo91@gmail.com>
2021-10-15 12:05:23 +0200

    :construction: godot working

--- packages/app/src/nodes/camera.gd
+++ packages/app/src/nodes/camera.gd
@@ -1,12 +1,53 @@
 extends Camera
 
-var mouse_sens = 0.3
-var camera_anglev = 0
+enum Mode {ROTATE, TRANSLATE}
 
-func _input(event):         
-	if event is InputEventMouseMotion:
-		rotate_y(deg2rad(-event.relative.x * mouse_sens))
-		var changev =- event.relative.y * mouse_sens
-		if camera_anglev + changev > -50 and camera_anglev + changev < 50:
-			camera_anglev += changev
-			rotate_x(deg2rad(changev))
+var mode = Mode.ROTATE
+var tween: Tween = null
+
+func _ready():
+	look_at_from_position(
+		Vector3(-8, 8, -8),
+		Vector3(0, 0, 0),
+		Vector3(0, 1, 0)
+	)
+
+func _input(event):
+	if event.is_action_pressed("ui_select"):
+		mode = Mode.TRANSLATE if mode == Mode.ROTATE else Mode.ROTATE
+	elif event.is_action_pressed("ui_left") and tween == null:
+		self._on_translate_begin(Vector2(-1, 0))
+	elif event.is_action_pressed("ui_right") and tween == null:
+		self._on_translate_begin(Vector2(1, 0))
+	elif event.is_action_pressed("ui_up") and tween == null:
+		self._on_translate_begin(Vector2(0, -1))
+	elif event.is_action_pressed("ui_down") and tween == null:
+		self._on_translate_begin(Vector2(0, 1))
+		
+	if event is InputEventMouseMotion:		
+		if event.relative.length() < 0.5 or tween:
+			return
+		
+		if mode == Mode.ROTATE:
+			return
+		if mode == Mode.TRANSLATE:
+			self._on_translate_begin(event.relative.normalized())
+
+func _on_translate_begin(t: Vector2):
+	tween = Tween.new()
+	add_child(tween)
+	tween.connect("tween_completed", self, "_on_translate_end")
+	tween.interpolate_property(
+		self,
+		"translation",
+		Vector3(self.translation.x, self.translation.y, self.translation.z),
+		Vector3(self.translation.x - t.y, self.translation.y, self.translation.z + t.x).round(),
+		1,
+		Tween.TRANS_QUART,
+		Tween.EASE_IN_OUT
+	)
+	tween.start()
+
+func _on_translate_end(_object, _key):
+	tween.queue_free()
+	tween = null
--- packages/app/src/scenes/board.gd
+++ packages/app/src/scenes/board.gd
@@ -3,24 +3,39 @@ extends Spatial
 var camera_scene = preload("res://src/nodes/camera.tscn")
 
 func _ready():
+	randomize()
+	
 	var camera = camera_scene.instance()
 	add_child(camera)
 	
 	for i in range(3):
 		for j in range(3):
 			for k in range(3):
-				var tile = get_tile("terrain") as CSGBox
-				tile.translation.x = -1 + i
-				tile.translation.y = -1 + j
-				tile.translation.z = -1 + k
-				
-				var material = tile.material as SpatialMaterial
-				material.albedo_color = Color(randf(), randf(), randf())
-				
-				add_child(tile)
+				if randi() % 2 == 0:
+					var tile = get_tile("terrain") as CSGBox
+					tile.translation.x = -1 + i
+					tile.translation.y = -1 + j
+					tile.translation.z = -1 + k
+					
+					tile.material = SpatialMaterial.new()
+					var material = tile.material as SpatialMaterial
+					material.albedo_color = Color(randf(), randf(), randf())
+					
+					add_child(tile)
+				else:
+					var tile = get_tile("warrior") as CSGSphere
+					tile.translation.x = -1 + i
+					tile.translation.y = -1 + j
+					tile.translation.z = -1 + k
+					
+					tile.material = SpatialMaterial.new()
+					var material = tile.material as SpatialMaterial
+					material.albedo_color = Color(randf(), randf(), randf())
+					
+					add_child(tile)
 	
 	pass
 	
 func get_tile(name: String):
-	return load("res://src/tiles/terrain.tscn").instance()
+	return load("res://src/tiles/" + name + ".tscn").instance()
 	
